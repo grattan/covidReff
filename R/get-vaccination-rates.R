4351f79603239b3859ed0b616b12fffdb4b6daae
HughParsonage <hugh.parsonage@gmail.com>
2021-07-03 15:01:53 +1000

    Avoiding warnings NOTES

--- R/get-vaccination-rates.R
+++ R/get-vaccination-rates.R
@@ -6,8 +6,6 @@
 #' @param age_rate a named vector with levels for 'under12', 'under40', 'under60',
 #' 'under80', 'over80'; eg: c(under12 = 0.00, under40 = 0.70, under60 = 0.90, under80 = 0.95, over80  = 0.95)
 #'
-#' @importFrom dplyr mutate summarise pull
-#' @importFrom stats weighted.mean
 #'
 #' @export
 
